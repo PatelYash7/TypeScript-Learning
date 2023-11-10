b8713eb4e372b4e9534fb22327888b30306ecfc9
PatelYash7 <yashpate0101@gmail.com>
2023-11-10 18:15:50 +0530

    array done

---
+++ array.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHero = [];
+// we can also write the above syntax in this manner 
+var cricketer = [];
+// Both the way of writing the code is best, no good  or bad, all is good.
+superHero.push("Yash");
---
+++ array.ts
@@ -0,0 +1,19 @@
+const superHero: string[] = [];
+
+// we can also write the above syntax in this manner 
+
+const cricketer: Array<String> = [];
+// Both the way of writing the code is best, no good  or bad, all is good.
+
+superHero.push("Yash");
+
+type User ={
+    name:string
+    isActive:boolean
+}
+const allUsers: User[]=[];
+ 
+allUsers.push({name:"Yash",isActive:true})
+
+
+export {}
