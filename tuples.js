fc8f319bcd0cbb44646e93b0595382e9d2fe1e4e
PatelYash7 <yashpate0101@gmail.com>
2023-11-11 12:45:36 +0530

    Tuples done

---
+++ tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User;
+User = ["Ys", 8798, true];
+//Used in Api calls 
+var rgb = [255, 178, 114];
+var item1 = [465, "Hello"];
---
+++ tuples.ts
@@ -0,0 +1,16 @@
+let User:[string,number,boolean]
+
+User=["Ys",8798,true];  
+//Used in Api calls 
+
+let rgb:[number,number,number]=[255,178,114]
+
+type User1=[number,string]
+
+let item1:User1=[465,"Hello"]
+
+// here we can push any item in User1 so it follow array structure, 
+// so protocol to use tuples is broken
+
+
+export {}
